346ebc77ee9f10625889a1c8e657e461fc0eb082
Yahya Alataş <yahya.alatas.mdbf17@iste.edu.tr>
2022-01-02 23:03:04 +0300

    set new itemse

--- src/Components/CustomButton/CustomInputs/CustomButton.tsx
+++ src/Components/CustomButton/CustomInputs/CustomButton.tsx
@@ -3,19 +3,18 @@ import {Text, View} from 'react-native';
 import {TextInput, TouchableOpacity} from 'react-native-gesture-handler';
 import styles from './styles';
 
-type IProps={
-  title:string,
-  loading:boolean,
-  disabled:boolean,
-  primary?:boolean,
-  danger?:boolean,
-  secondry?:boolean 
-  
-}
+type IProps = {
+  title: string;
+  loading: boolean;
+  disabled: boolean;
+  primary?: boolean;
+  danger?: boolean;
+  secondry?: boolean;
+};
 interface IState {
   isFoccused: boolean;
 }
-export default class CustomButton extends Component<IProps, IState>{
+export default class CustomButton extends Component<IProps, IState> {
   constructor(prop: IProps) {
     super(prop);
     this.state = {
@@ -23,25 +22,26 @@ export default class CustomButton extends Component<IProps, IState>{
     };
   }
 
-  getBgColor(){
-    if(this.props.secondry){
-      return "#31b554"
-    }
-    else if(this.props.primary){
-      return "#31b554"
-    }
-    else if(this.props.danger){
-      return "#e02b6d"
+  getBgColor() {
+    if (this.props.secondry) {
+      return '#31b554';
+    } else if (this.props.primary) {
+      return '#31b554';
+    } else if (this.props.danger) {
+      return '#e02b6d';
     }
   }
   render() {
     return (
       <TouchableOpacity
         disabled={this.props.disabled}
-        style={[styles.wrapper,{backgroundColor:this.getBgColor()}]}
-        
-      >
-          <Text style={{color:this.props.disabled?"grey":"black"}}>{this.props.title}</Text>
+        style={[styles.wrapper, {backgroundColor: this.getBgColor()}]}>
+        <Text style={{
+          color: this.props.disabled ? 'grey' : 'black',
+          fontFamily:"Rubik-VariableFont_wght"
+          }}>
+          {this.props.title}
+        </Text>
       </TouchableOpacity>
     );
   }
